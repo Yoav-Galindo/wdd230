6ec2ee5e2f3180965c768a6d808df6cd2bc9db40
Yoav-Galindo <gal22005@byui.edu>
2023-05-20 09:25:39 -0600

    test2

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -94,6 +94,6 @@ hamburgerBtnSpot.onclick = toggleMenu;
 
 const bannerElement = document.querySelector('.banner');
 const today = dayName;
-if (today === "Monday" || today=== "Saturday") {
+if (today === "Monday" || today=== "Tuesday") {
 	bannerElement.style = 'display:block';
 }
