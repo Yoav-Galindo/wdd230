54dea1c9af74ab4ad204e9e5e18d990203a2bbf1
Yoav-Galindo <gal22005@byui.edu>
2023-06-03 23:41:37 -0600

    chamberLazyLoad

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -97,3 +97,40 @@ const today = dayName;
 if (today === "Monday" || today=== "Tuesday") {
 	bannerElement.style = 'display:block';
 }
+// ----------------------------------Lazy Loading----------------------------//
+// Select the images I want to load by the selector querySelectorAll selecting these by the html element img and the attribute [data-src].
+const imgToLoad = document.querySelectorAll("img[data-src]");
+
+// These are the options to load the images.
+const imageOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 200px 0px",
+};
+
+// This fuction is going to remove the data-src attribute and replace for src attribute.
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// Check if the Intersection Observer is supported.
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+    // Use the image options.
+  }, imageOptions);
+  imgToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imgToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
